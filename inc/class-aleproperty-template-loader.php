3dc2f158edc80bc3ef8915ab518b1a09054ee01a
tyshchukRoman <roman.t@codelibry.dev>
2025-05-18 22:11:50 +0300

    feat: add template loading for single and archive

--- inc/class-aleproperty-template-loader.php
+++ inc/class-aleproperty-template-loader.php
@@ -15,19 +15,32 @@ class AlepropertyTemplateLoader extends Gamajo_Template_Loader  {
   }
 
   public function aleproperty_templates($template) {
+    foreach (['property', 'agent'] as $post_type) {
+      if(is_post_type_archive($post_type)) {
+        return $this->archive_template($post_type);
+      }
 
-    if(is_post_type_archive('property')) {
-      return $this->property_archive();
+      if(is_single() && get_post_type() === $post_type) {
+        return $this->single_template($post_type);
+      }
     }
 
     return $template;
   }
 
-  private function property_archive() {
-    $theme_files = ['archive-property.php', 'aleproperty/archive-property.php'];
+  private function archive_template(string $post_type) {
+    $theme_files = ["archive-$post_type.php", "aleproperty/archive-$post_type.php"];
 
     $exist = locate_template($theme_files, false);
 
-    return $exist != '' ? $exist : ALEPROPERTY_PATH.'templates/archive-property.php';
+    return $exist != '' ? $exist : ALEPROPERTY_PATH . "templates/archive-$post_type.php";
+  }
+
+  private function single_template(string $post_type) {
+    $theme_files = ["single-$post_type.php", "aleproperty/single-$post_type.php"];
+
+    $exist = locate_template($theme_files, false);
+
+    return $exist != '' ? $exist : ALEPROPERTY_PATH . "templates/single-$post_type.php";
   }
 }
---
+++ templates/archive-agent.php
@@ -0,0 +1,7 @@
+<?php
+
+get_header();
+
+echo "Archive for agent";
+
+get_footer();
